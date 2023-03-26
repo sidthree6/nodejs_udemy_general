68957f2d4051ee67adb7bd8d738c73854b490016
sidthree6 <psiddhu001@gmail.com>
2023-03-26 14:33:13 -0400

    Finished section 3 and also renamed folder.

--- 3-section/routes.js
+++ 3-section/routes.js
@@ -1,10 +1,10 @@
-const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+
+const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
+
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -33,7 +33,7 @@ const server = http.createServer((req, res) => {
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
-    res.end();    
-});
+    res.end();
+};
 
-server.listen(port=3000);
+exports.handler = requestHandler;
